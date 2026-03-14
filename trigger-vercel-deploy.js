98a2b54058f1772a29c30168593e5651ead9cc6b
natiberhanu0583 <natiberhanu0583@gmail.com>
2026-03-14 09:11:07 -0700

    Trigger Vercel deployment - tabbed interface ready

---
+++ trigger-vercel-deploy.js
@@ -0,0 +1,4 @@
+// This file is created to trigger Vercel deployment
+// It will be removed after deployment
+const deploymentTrigger = new Date().toISOString();
+console.log('Triggering Vercel deployment at:', deploymentTrigger);
